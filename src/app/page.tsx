c7960355ace7c4f88b47bac6b7e70a373229845a
samueljakshtis <sam.jakshtis@gmail.com>
2025-01-09 22:33:08 -0500

    trying

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import { Mainpage } from "@/components/page/Mainpage"
-import { Html, Head, Main, NextScript } from 'next/document';
+import Script from "next/script"
 
 export default function Home() {
   return (
     <>
-      <script
-        async
-        defer
-        data-website-id="f8190cf5-691d-41e2-bce8-0917c93c6c74"
-        src="https://cloud.umami.is/script.js"
-      ></script>
-
-      <Mainpage />
+      <html>
+        <head>
+          <Script
+            async
+            defer
+            data-website-id="f8190cf5-691d-41e2-bce8-0917c93c6c74"
+            src="https://cloud.umami.is/script.js"
+          />
+        </head>
+        <body>
+          <Mainpage />
+        </body>
+      </html>
     </>
   )
 }
