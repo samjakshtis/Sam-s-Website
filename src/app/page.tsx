17b3039791216714ed55446706e00e81e2bc9e73
samueljakshtis <sam.jakshtis@gmail.com>
2025-01-09 22:38:01 -0500

    last

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,7 @@ import Script from "next/script"
 export default function Home() {
   return (
     <>
-      <html>
-        <head>
-          <Script
-            async
-            defer
-            data-website-id="f8190cf5-691d-41e2-bce8-0917c93c6c74"
-            src="https://cloud.umami.is/script.js"
-          />
-        </head>
-        <body>
-          <Mainpage />
-        </body>
-      </html>
+      <Mainpage />
     </>
   )
 }
